77da540552e65ec74da69385ffbdd042b845fd22
mitsugeek <mitsu322@gmail.com>
2021-02-11 00:33:41 +0900

    レイアウト調整

--- resources/views/liff/home.blade.php
+++ resources/views/liff/home.blade.php
@@ -14,6 +14,7 @@
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
     <script charset="utf-8" src="https://static.line-scdn.net/liff/edge/2/sdk.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </head>
 <style>
     body {
--- resources/views/liff/taionlist.blade.php
+++ resources/views/liff/taionlist.blade.php
@@ -7,18 +7,34 @@
     <title>体温一覧</title>
 </head>
 <body>
-    <table>
-    @foreach($list as $row)
-        <tr>
-            <td>{{ $row->line_name }}</td>
-            <td>{{ $row->name_sei }}</td>
-            <td>{{ $row->name_mei }}</td>
-            <td>{{ $row->created_at }}</td>
-            <td><img src="{{ $row->url }}" width="100px"/></td>
-        </tr>
-    @endforeach
-    </table>
-    
-    {{ $list->links() }}
+    <div class="container">
+        <div class="row">
+            <div class="col">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <table>
+                    @foreach($list as $row)
+                    <tr>
+                        <td>{{ $row->line_name }}</td>
+                        <td>{{ $row->name_sei }}</td>
+                        <td>{{ $row->name_mei }}</td>
+                        <td>{{ $row->created_at }}</td>
+                        <td><img src="{{ $row->url }}" width="100px"/></td>
+                    </tr>
+                    @endforeach
+                </table>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                {{ $list->links() }}
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>    
 </body>
 </html>
