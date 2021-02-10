9a04e1f6fc3bf6596532433787946febdb26420e
mitsugeek <mitsu322@gmail.com>
2021-02-11 01:28:05 +0900

    postのnameに$loop->indexを追加

--- resources/views/liff/userlist.blade.php
+++ resources/views/liff/userlist.blade.php
@@ -13,11 +13,11 @@
         @csrf
         <table class="table">
             @foreach($list as $row)
-            <input type="hidden" name="row[][line_id]" value="{{$row->line_id}}" />
+            <input type="hidden" name="row[{{$loop->index}}][line_id]" value="{{$row->line_id}}" />
             <tr>
                 <td>{{$row->line_name}}</td>
-                <td><input type="text" name="row[][name_sei]" value="{{$row->name_sei}}" /></td>
-                <td><input type="text" name="row[][name_mei]" value="{{$row->name_mei}}" /></td>
+                <td><input type="text" name="row[{{$loop->index}}]name_sei]" value="{{$row->name_sei}}" /></td>
+                <td><input type="text" name="row[{{$loop->index}}][name_mei]" value="{{$row->name_mei}}" /></td>
             </tr>
             @endforeach
         </table>
